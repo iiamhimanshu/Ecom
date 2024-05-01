ccecdf4245671357ff7e227b9bd2d413049d378e
iiamhimanshu <himanshu5rahul@gmail.com>
2024-05-01 16:21:27 +0530

    1

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DescriptionBox.css';
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box-fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online platform where users can browse, search for, and purchase a wide range of products or services. These websites typically feature intuitive navigation, product categories, search functionalities, and secure payment gateways to ensure a smooth shopping experience. Customers can explore detailed product descriptions, images, reviews, and ratings to make informed buying decisions. E-commerce sites often offer personalized recommendations, promotions, and discounts to enhance customer loyalty. With convenient shipping options and easy return policies, these platforms aim to provide convenience and satisfaction to shoppers, making online shopping a seamless and enjoyable experience. Additionally, they foster global market access, allowing businesses to reach a wider audience and expand their customer base.</p>
+            
+        </div>
+    </div>
+  )
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useContext, useState } from "react";
+import "./Navbar.css"
+
+import logo from "..//Assets/logo.png"
+import cart_icon from "..//Assets/cart_icon.png"
+import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+
+function Navbar(){
+
+    var[menu,setMenu] = useState("shop");
+    const{getTotalCartItems} = useContext(ShopContext);
+
+    return(
+        <div  className="Navbar">
+            <div className="nav-logo">
+                
+                <img src={logo} alt="" />
+                <p>SHOPPER</p>
+
+            </div>
+            <ul className="nav-menu">
+                <li onClick={()=>setMenu("shop")}><Link to='/'>Shop</Link>{menu === "shop"?<hr/> : <></>}</li>
+                <li onClick={()=>setMenu("men")}><Link to='/mens'>Men</Link>  {menu === "men"?<hr/> : <></>}</li>
+                <li onClick={()=>setMenu("women")}><Link to="womens">Women</Link> {menu === "women"?<hr/> : <></>}</li>
+                <li onClick={()=>setMenu("kids")}><Link to='/kids'> Kids </Link>{menu === "kids"?<hr/> : <></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <Link to='/login'><button>Login</button></Link>  
+                <Link to='/cart'><img src={cart_icon} alt="" /></Link> 
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
